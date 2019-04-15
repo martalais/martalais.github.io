ebf1cfbfeb78fe58401e9ff97acf235b3a87e142
Arthur Henrique <arthurhenrique@live.com>
2019-04-15 19:07:44 -0300

    Implement automatic change to my age
    This code will avoid the presentation page to be outdated, updating how old you are every year.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+// When the page is ready, do the following...
+$(document).ready(function() {
+	
+  /* ************************************************************************ *
+   * Starts logic regarding automatic age change on portifolio's presentation
+   * ************************************************************************ */
+  var iWasBornIn		= 1998;
+  var currentYear		= new Date().getFullYear();
+  var myActualYear	= currentYear - iWasBornIn;
+  
+	$('#myAtualAge').text(myActualYear);
+  /* ************************************************************************ */
+  
+});
